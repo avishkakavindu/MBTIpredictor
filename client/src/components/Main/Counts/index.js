efa1db3b36eedff953b781e4e84d928d686b756a
avishkakavindu <avishkakavindud@gmail.com>
2022-12-24 18:07:39 +0530

    Counts added

--- client/src/components/Main/Counts/index.js
+++ client/src/components/Main/Counts/index.js
@@ -1,18 +1,31 @@
-import './assets/styles/styles.css';
+import { useEffect, useState } from 'react';
 import CountUp from 'react-countup';
 
+import { getPredictionCounts } from '../../../services/prediction.service';
+
+import './assets/styles/styles.css';
+
 const Count = () => {
-  const userCount = 232;
-  const numberOfTests = 521;
-  const numberOfSuccessfulPredictions = 1463;
-  const numberOfPredictions = 15;
+  const [counts, setCounts] = useState({});
+
+  // on load
+  useEffect(() => {
+    getPredictionCounts().then(
+      (response) => {
+        setCounts(response.data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
 
   return (
     <section id='counts' className='counts'>
       <div className='container'>
         <div className='row counters'>
           <div className='col-lg-3 col-6 text-center'>
-            <CountUp start={0} end={userCount} delay={0}>
+            <CountUp start={0} end={counts.user_count} delay={0}>
               {({ countUpRef }) => (
                 <span ref={countUpRef} className='purecounter'></span>
               )}
@@ -21,7 +34,7 @@ const Count = () => {
           </div>
 
           <div className='col-lg-3 col-6 text-center'>
-            <CountUp start={0} end={numberOfTests} delay={0}>
+            <CountUp start={0} end={counts.number_of_predictions} delay={0}>
               {({ countUpRef }) => (
                 <span ref={countUpRef} className='purecounter'></span>
               )}
@@ -30,7 +43,11 @@ const Count = () => {
           </div>
 
           <div className='col-lg-3 col-6 text-center'>
-            <CountUp start={0} end={numberOfSuccessfulPredictions} delay={0}>
+            <CountUp
+              start={0}
+              end={counts.successful_prediction_count}
+              delay={0}
+            >
               {({ countUpRef }) => (
                 <span ref={countUpRef} className='purecounter'></span>
               )}
@@ -39,7 +56,11 @@ const Count = () => {
           </div>
 
           <div className='col-lg-3 col-6 text-center'>
-            <CountUp start={0} end={numberOfPredictions} delay={0}>
+            <CountUp
+              start={0}
+              end={counts.unsuccessful_prediction_count}
+              delay={0}
+            >
               {({ countUpRef }) => (
                 <span ref={countUpRef} className='purecounter'></span>
               )}
--- client/src/services/prediction.service.js
+++ client/src/services/prediction.service.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
-import { authHeader } from './auth-header.service';
 
 const baseUrl = 'http://localhost:8000/api/v1';
 
 export const getRandomPredictions = (n = 5) => {
   return axios.get(`${baseUrl}/predictions/${n}`);
+};
+
+export const getPredictionCounts = () => {
+  return axios.get(`${baseUrl}/counts/`);
 };
